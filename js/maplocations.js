8f2daa56df416f88f91053b58e795e65d795e2f3
James Kisby <james.kisby@hotmail.com>
2017-01-19 05:22:53 +0000

    Add filter search

--- js/maplocations.js
+++ js/maplocations.js
@@ -12,7 +12,7 @@ var initialLocations = [
 			lat: -28.363,
 			lng: 131.044
 		},
-		title: "title1",
+		title: "random",
 		img: "img/img1.jpg"
 	},
 	{
@@ -20,7 +20,7 @@ var initialLocations = [
 			lat: -23.363,
 			lng: 131.044
 		},
-		title: "title1",
+		title: "london",
 		img: "img/img1.jpg"
 	},
 	{
@@ -28,7 +28,7 @@ var initialLocations = [
 			lat: -20.363,
 			lng: 131.044
 		},
-		title: "title1",
+		title: "bridge",
 		img: "img/img1.jpg"
 	},
 	{
@@ -36,7 +36,10 @@ var initialLocations = [
 			lat: -26.363,
 			lng: 131.044
 		},
-		title: "title1",
+		title: "words",
 		img: "img/img1.jpg"
 	}
 ];
+
+var landmarks = "https://maps.googleapis.com/maps/api/place/nearbysearch/"
+				+ "json?location=%lat%,%lng%&radius=150&key=AIzaSyCdsY4LFODHr3YVRwoK4XClAUBQrMS8h9I"
--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 var viewModel = function(map){
 	var self = this;
 	self.locationlist = ko.observableArray([]);
+	self.filter = ko.observable();
+	self.filteredList = ko.observableArray([]);
 
 	initialLocations.forEach(function(mark){
 		var m = new google.maps.Marker({
-				position: mark.pos,
-				map: map,
-				title: mark.title
-			});
+			position: mark.pos,
+			map: map,
+			title: mark.title
+		});
 		self.locationlist.push(m);
 	});
 
@@ -16,22 +18,37 @@ var viewModel = function(map){
 
 	this.setLocation = function(location){
 		self.currentLocation(location);
-	}
+	};
 
 	this.setMapLocation = function(location){
 		console.log(location.getTitle());
 		var center = new google.maps.LatLng(location.getPosition().lat(), location.getPosition().lng());
 		map.panTo(center);
-	}
+	};
 
-}
+	this.filterList = ko.computed(function(){
+		var filter = self.filter();
+		if(!filter){
+			return self.locationlist();
+		}else{
+			return ko.utils.arrayFilter(self.locationlist(), function(location){
+				return location.getTitle().toLowerCase().startsWith(filter);
+			});
+		}
+	});
 
+	this.noResults = ko.computed(function(){
+		if(self.filterList().length === 0){
+			return true;
+		}else{
+			return false;
+		}
+	});
+	
+};
+console.log(landmarks);
 
 function initMap() {
-	var timeout = setTimeout(function(){
-        alert("Failed to get wikipedia resources");
-    }, 4000);
-
 	var map = new google.maps.Map(document.getElementById('map'), {
 		 zoom: 7,
 		 center: initialLocations[0].pos
